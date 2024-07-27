787b1ec9228b6bd59473392690f7b03b4e2c98e7
stenzr <rkrohitkr01@gmail.com>
2024-07-27 23:13:42 +0530

    [add] setup and [add] requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# Read the requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+setup(
+    name="concurrent-scripts",
+    version="0.1.0",
+    description="A package to run multiple instance of the same script.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="",
+    author="Rohit Kumar",
+    author_email="rkrohitkr01@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'concurrent-scripts=concurrent_scripts.__main__:main',
+        ],
+    },
+    install_requires=install_requires,
+    python_requires='>=3.6',
+    extras_require={
+        'test': ['unittest', 'coverage'],
+    },
+)
